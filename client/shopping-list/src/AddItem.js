c148deb0a4eb7fdaac19765bbfd62e710a59aa11
Oliver <oliver.nic012@gmail.com>
2022-09-14 13:10:55 -0500

    Create separate component for adding items.

---
+++ client/shopping-list/src/AddItem.js
@@ -0,0 +1,50 @@
+
+import React from 'react';
+
+class AddItem extends React.Component {
+
+   constructor(props) {
+      super(props);
+      this.state = {
+         text: ''
+      };
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+   }
+
+   handleChange(e) {
+      this.setState({ text: e.target.value });
+   }
+
+   handleSubmit(e) {
+      e.preventDefault();
+      const value = this.state.text;
+      if (value.length > 0) {
+         if (this.props.onAdd) {
+            this.props.onAdd(value);
+         }
+         this.setState(state => ({
+            text: ''
+         }));
+      }
+   }
+
+   render() {
+      return (
+         <form onSubmit={this.handleSubmit}>
+            <label htmlFor="new-item" className="d-none">Add Item</label>
+            <div className="input-group mb-3">
+               <button id="add-item" type="submit" className="btn btn-outline-secondary bg-primary text-light">
+                  <i className="fa-plus fa-xl"></i> 
+               </button>
+               <input id="new-item" type="text" className="form-control bg-dark text-light border-primary" 
+                     onChange={this.handleChange}
+                     value={this.state.text}
+                     placeholder="" aria-label="" aria-describedby="add-item"/>
+            </div>
+         </form>
+      );
+   }
+}
+
+export default AddItem;
--- client/shopping-list/src/List.js
+++ client/shopping-list/src/List.js
@@ -1,64 +1,32 @@
 import React from 'react';
+import AddItem from './AddItem';
 
 class List extends React.Component {
 
    constructor(props) {
       super(props);
       this.state = {
-         items: [],
-         text: ''
+         items: []
       };
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
+      this.addItem = this.addItem.bind(this);
    }
 
-   handleChange(e) {
-      this.setState({ text: e.target.value });
+   addItem(value) {
+      this.setState(state => ({
+         items: state.items.concat(value)
+      }));
    }
 
-   handleSubmit(e) {
-      e.preventDefault();
-      const value = this.state.text;
-      console.log("submitted");
-      if (value.length > 0) {
-         this.setState(state => ({
-            items: state.items.concat(value),
-            text: ''
-         }));
-      }
-   }
-
-   getAddBtn() {
-      return (
-         <form onSubmit={this.handleSubmit}>
-            <label htmlFor="new-item" className="d-none">Add Item</label>
-            <div className="input-group mb-3">
-               <button id="add-item" type="submit" className="btn btn-outline-secondary bg-primary text-light">
-                  <i className="fa-plus fa-xl"></i> 
-               </button>
-               <input id="new-item" type="text" className="form-control bg-dark text-light border-primary" 
-                     onChange={this.handleChange}
-                     value={this.state.text}
-                     placeholder="" aria-label="" aria-describedby="add-item"/>
-            </div>
-         </form>
-      );
-   }
-
-   getItems() {
+   render() {
       const listItems = this.state.items.map((value, index) => 
          <li key={index}>{value}</li>
       );
       return (
-         <ul>{listItems}</ul>
+         <div>
+            <AddItem onAdd={this.addItem}></AddItem>
+            <ul>{listItems}</ul>
+         </div>
       );
-   }
-
-   render() {
-      return [
-         this.getAddBtn(),
-         this.getItems()
-      ];
    }
 }
 
