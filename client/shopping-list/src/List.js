de37c3beb4871eed2d3c44133075de762c5ea383
Oliver <oliver.nic012@gmail.com>
2022-09-13 23:23:18 -0500

    Ability to add items to list.

--- client/shopping-list/src/List.js
+++ client/shopping-list/src/List.js
@@ -5,13 +5,43 @@ class List extends React.Component {
    constructor(props) {
       super(props);
       this.state = {
-         items: []
+         items: [],
+         text: ''
       };
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+   }
+
+   handleChange(e) {
+      this.setState({ text: e.target.value });
+   }
+
+   handleSubmit(e) {
+      e.preventDefault();
+      const value = this.state.text;
+      console.log("submitted");
+      if (value.length > 0) {
+         this.setState(state => ({
+            items: state.items.concat(value),
+            text: ''
+         }));
+      }
    }
 
    getAddBtn() {
       return (
-         <button class="btn bg-primary text-light">Add</button>
+         <form onSubmit={this.handleSubmit}>
+            <label htmlFor="new-item" className="d-none">Add Item</label>
+            <div className="input-group mb-3">
+               <button id="add-item" type="submit" className="btn btn-outline-secondary bg-primary text-light">
+                  <i className="fa-plus fa-xl"></i> 
+               </button>
+               <input id="new-item" type="text" className="form-control bg-dark text-light border-primary" 
+                     onChange={this.handleChange}
+                     value={this.state.text}
+                     placeholder="" aria-label="" aria-describedby="add-item"/>
+            </div>
+         </form>
       );
    }
 
