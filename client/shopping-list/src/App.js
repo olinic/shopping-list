d020fafcf09f9f2fef4d104f002aca0c50f5a4a9
Oliver <oliver.nic012@gmail.com>
2022-09-14 23:33:49 -0500

    Use bootstrap to style header.

--- client/shopping-list/src/App.js
+++ client/shopping-list/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App bg-dark text-light">
       <div className="container text-center">
-        <h1>Shopping List</h1>
+        <h1 className="mt-4">Shopping List</h1>
         <List></List>
       </div>
     </div>
