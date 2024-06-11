b4dfdc16b993e3e15ff563400a2a0429bea8fafa
Oliver <oliver.nic012@gmail.com>
2024-06-10 22:13:54 -0500

    Fix tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,8 @@
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  const root = createRoot(div);
+  root.render(<App />);
 });
--- src/List.test.js
+++ src/List.test.js
@@ -1,93 +1,48 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, cleanup, screen, fireEvent } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import List from './List';
 
+let storage = {};
+
 describe('List', () => {
+   beforeAll(() => {
+      global.Storage.prototype.setItem = jest.fn((key, value) => {
+         storage[key] = value;
+      });
+      global.Storage.prototype.getItem = jest.fn((key) => storage[key]);
+   });
+
+   beforeEach(() => {
+      cleanup();
+      storage = {};
+   });
+
    test('renders List component', () => {
       render(<List />);
       expect(screen.getByRole('textbox')).toBeInTheDocument();
    });
 
-   test('add List Item', () => {
-      render(<List />);
-      const input = "silly text";
-      userAddItem(input);
-      expect(screen.getByDisplayValue(input)).toBeInTheDocument();
-   });
-
-   test('add List Item to start', () => {
-      render(<List />);
-      userAddItem("a");
-      userAddItem("z");
-      expectListItems(["z", "a"]);
-   });
-
-   test('add List Item to end', () => {
-      render(<List />);
-      userAddItem("a");
-      userAddItemToEnd("z");
-      expectListItems(["a", "z"]);
-   });
-
-   function userAddItemToEnd(text) {
+   async function userAddItemToEnd(text) {
       const addItemToEnd = {name: "Add item to end"};
-      userEvent.click(screen.getByRole('textbox', addItemToEnd));
-      userEvent.keyboard(text);
-      userEvent.click(screen.getByRole('button', addItemToEnd));
+      await userEvent.click(screen.getByRole('textbox', addItemToEnd));
+      await userEvent.keyboard(text);
+      await userEvent.click(screen.getByRole('button', addItemToEnd));
    }
 
-   function userAddItem(text) {
+   async function userAddItem(text) {
       const addItem = {name: /add item to start/i};
-      userEvent.click(screen.getByRole('textbox', addItem));
-      userEvent.keyboard(text);
-      userEvent.click(screen.getByRole('button', addItem));
+      await userEvent.click(screen.getByRole('textbox', addItem));
+      await userEvent.keyboard(text);
+      await userEvent.click(screen.getByRole('button', addItem));
    }
 
-   test('remove List Item', () => {
-      render(<List />);
+   test('add List Item', async () => {
       const input = "silly text";
-      userAddItem(input);
-      userEvent.click(screen.getByRole('button', {name: /delete item/i}));
-      expect(screen.queryByDisplayValue(input)).toBeNull();
-   });
-
-   test('edit List Item', () => {
       render(<List />);
-      userAddItem("silly");
-      userEvent.click(screen.getByRole('textbox', {name: /list item/i}));
-      userEvent.keyboard("s");
-      expect(screen.getByDisplayValue("sillys")).toBeInTheDocument();
-   });
-
-   test('complete List Item', () => {
-      render(<List />);
-      const input = "silly text";
-      userAddItem(input);
-      userCompleteItem();
-      const element = screen.getByDisplayValue(input)
-      expect(element).toBeInTheDocument();
-      expect(element).toHaveClass("text-st");
-   });
-
-   function userCompleteItem() {
-      userEvent.click(screen.getByRole('button', {name: /complete list item/i}));
-   }
-
-   test('completed List Item shows in Checked Items section by default', () => {
-      render(<List />);
-      const input = "silly text";
-      userAddItem(input);
-      userCompleteItem();
-      expect(screen.getByText("Checked Items")).toBeInTheDocument();
-   });
-
-   test('completed List Item are in alphabetical order in Checked Items section', () => {
-      render(<List />);
-      userAddItems(["b", "c", "a"]);
-      userCompleteAllItems();
-      expectListItems(["a", "b", "c"]);
+      await userAddItem(input);
+      expect(screen.getByDisplayValue(input)).toBeInTheDocument();
    });
 
    function expectListItems(expectedList) {
@@ -99,58 +54,119 @@ describe('List', () => {
       }
    }
 
-   function userCompleteAllItems() {
-      const elements = screen.getAllByRole('button', {name: /complete list item/i})
-      elements.forEach(element => userEvent.click(element));
-   }
+   test('add List Item to start', async () => {
+      render(<List />);
+      await userAddItem("a");
+      await userAddItem("z");
+      expectListItems(["z", "a"]);
+   });
 
-   test('completed List Item shows in List section when sort view is enabled', () => {
+   test('add List Item to end', async () => {
+      render(<List />);
+      await userAddItem("a");
+      await userAddItemToEnd("z");
+      expectListItems(["a", "z"]);
+   });
+
+   test('remove List Item', async () => {
       render(<List />);
       const input = "silly text";
-      enableSortView();
-      userAddItem(input);
-      userCompleteItem();
+      await userAddItem(input);
+      await userEvent.click(screen.getByRole('button', {name: /delete item/i}));
+      expect(screen.queryByDisplayValue(input)).toBeNull();
+   });
+
+   test('edit List Item', async () => {
+      render(<List />);
+      await userAddItem("silly");
+      await userEvent.click(screen.getByRole('textbox', {name: /list item/i}));
+      await userEvent.keyboard("s");
+      expect(screen.getByDisplayValue("sillys")).toBeInTheDocument();
+   });
+
+   test('complete List Item', async () => {
+      render(<List />);
+      const input = "silly text";
+      await userAddItem(input);
+      await userCompleteItem();
+      const element = screen.getByDisplayValue(input)
+      expect(element).toBeInTheDocument();
+      expect(element).toHaveClass("text-st");
+   });
+
+   async function userCompleteItem() {
+      await userEvent.click(screen.getByRole('button', {name: /complete list item/i}));
+   }
+
+   test('completed List Item shows in Checked Items section by default', async () => {
+      render(<List />);
+      const input = "silly text";
+      await userAddItem(input);
+      await userCompleteItem();
+      expect(screen.getByText("Checked Items")).toBeInTheDocument();
+   });
+
+   test('completed List Item are in alphabetical order in Checked Items section', async () => {
+      render(<List />);
+      await userAddItems(["b", "c", "a"]);
+      await userCompleteAllItems();
+      expectListItems(["a", "b", "c"]);
+   });
+
+   async function userCompleteAllItems() {
+      const elements = screen.getAllByRole('button', {name: /complete list item/i})
+      for (let element of elements) {
+         await userEvent.click(element);
+      }
+   }
+
+   test('completed List Item shows in List section when sort view is enabled', async () => {
+      render(<List />);
+      const input = "silly text";
+      await enableSortView();
+      await userAddItem(input);
+      await userCompleteItem();
       expect(screen.queryByText("Checked Items")).toBeNull();
    });
 
-   function enableSortView() {
-      userEvent.click(screen.getByRole('checkbox', {name: /sort view/i}));
+   async function enableSortView() {
+      await userEvent.click(screen.getByRole('checkbox', {name: /sort view/i}));
+   }
+
+   async function userAddItems(list) {
+      const reversed = list.slice().reverse();
+      for (const item of reversed) {
+         await userAddItem(item);
+      }
    }
 
    test('move buttons not present in default view', () => {
       expect(screen.queryAllByRole('button', {name: /move item/i}).length).toBe(0);
    });
 
-   test('move List Item down', () => {
+   test('move List Item down', async () => {
       render(<List />);
-      userAddItems(["a", "b", "c"]);
-      enableSortView();
-      moveItemDown(0);
+      await userAddItems(["a", "b", "c"]);
+      await enableSortView();
+      await moveItemDown(0);
       expectListItems(["b", "a", "c"]);
    });
 
-   function moveItemDown(idx) {
+   async function moveItemDown(idx) {
       const moveUpBtns = screen.getAllByRole('button', {name: /move item down/i});
-      userEvent.click(moveUpBtns[idx]);
+      await userEvent.click(moveUpBtns[idx]);
    }
 
-   function moveItemUp(idx) {
+   async function moveItemUp(idx) {
       const moveUpBtns = screen.getAllByRole('button', {name: /move item up/i});
-      userEvent.click(moveUpBtns[idx]);
+      await userEvent.click(moveUpBtns[idx]);
    }
 
-   function userAddItems(list) {
-      const reversed = list.slice().reverse();
-      for (const item of reversed) {
-         userAddItem(item);
-      }
-   }
-
-   test('move List Item up', () => {
+   test('move List Item up', async () => {
       render(<List />);
-      userAddItems(["a", "b", "c"]);
-      enableSortView();
-      moveItemUp(1);
+      await userAddItems(["a", "b", "c"]);
+      await enableSortView();
+      await moveItemUp(1);
       expectListItems(["b", "a", "c"]);
    });
 });
