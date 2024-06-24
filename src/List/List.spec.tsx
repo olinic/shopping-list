818770634ba3eae11a271cbac332307665b6e50b
Oliver <oliver.nic012@gmail.com>
2024-06-24 15:10:09 -0500

    Fix test

--- src/List/List.spec.tsx
+++ src/List/List.spec.tsx
@@ -1,6 +1,6 @@
 import { render, cleanup, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import * as StorageService from './Storage/StorageService';
+import * as StorageService from '../Storage/StorageService';
 
 import List from './List';
 
