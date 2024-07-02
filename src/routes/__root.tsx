43f01b5a6bfac7abb09631a2298193909f4fb9ac
Oliver <oliver.nic012@gmail.com>
2024-07-02 13:46:18 -0500

    Fix build issues

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -3,9 +3,10 @@ import React, { Suspense } from 'react'
 import { createRootRoute, Outlet } from '@tanstack/react-router'
 
 const TanStackRouterDevtools =
-  process.env.NODE_ENV === 'production'
-    ? () => null // Render nothing in production
-    : React.lazy(() =>
+  import.meta.env.PROD ? 
+    () => null // Render nothing in production
+    : 
+    React.lazy(() =>
         // Lazy load in development
         import('@tanstack/router-devtools').then((res) => ({
           default: res.TanStackRouterDevtools,
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,22 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig, mergeConfig } from 'vite'
+import { UserConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    TanStackRouterVite(),
-    react()
-  ],
-  base: "/shopping-list/",
+const vitestConfig: UserConfig = {
   test: {
     include: ['**/*.test.tsx'],
     globals: true,
     environment: "happy-dom",
     setupFiles: './src/setupTests.ts'
   }
-})
+};
+
+// https://vitejs.dev/config/
+export default mergeConfig(vitestConfig, defineConfig({
+  plugins: [
+    TanStackRouterVite(),
+    react()
+  ],
+  base: "/shopping-list/"
+}));
